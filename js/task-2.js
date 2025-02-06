b3aeb7f26850e28cc1550199e27dfd7a0cd7400d
Darina Boinik <darinaboinik@gmail.com>
2025-02-06 12:15:14 +0200

    add all

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength) {
-    if (message <= maxLength) {
+    if (message.lengthe <= maxLength) {
         return message;
     }
     return message.slice(0, maxLength) + "...";
--- js/task-4.js
+++ js/task-4.js
@@ -2,7 +2,7 @@ function getShippingCost(country) {
     switch (country) {
         case "China":
             return "Shipping to China will cost 100 credits";
-        case "chile":
+        case "Chile":
             return "Shipping to Chile will cost 250 credits";
         case "Australia":
             return "Shipping to Australia will cost 170 credits";
